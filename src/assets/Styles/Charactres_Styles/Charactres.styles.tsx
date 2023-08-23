ff94508b22e3e75b062aed891886b72a1870784e
Heisen <leonardopjh30@gmail.com>
2023-08-23 19:40:32 -0300

    feat(style): ajusted from scrren

--- src/assets/Styles/Charactres_Styles/Charactres.styles.tsx
+++ src/assets/Styles/Charactres_Styles/Charactres.styles.tsx
@@ -5,6 +5,10 @@ export const Wrapper_Charactres = styled.div`
   display: flex;
   flex-direction: column; /* Coloca os elementos em uma coluna */
   align-items: center; /* Centraliza o conteúdo verticalmente */
+
+  @media (max-width: 768px) {
+    align-items: center; /* Centraliza o conteúdo horizontalmente em dispositivos móveis */
+  }
 `;
 
 export const Title_Section_Card = styled.h2`
@@ -13,6 +17,7 @@ export const Title_Section_Card = styled.h2`
   font-weight: bold;
   margin-top: 30px;
 `;
+
 export const Character_Card = styled.div`
   width: 200px;
   height: 200px;
@@ -20,9 +25,21 @@ export const Character_Card = styled.div`
   &:hover {
     cursor: pointer;
   }
+
+  @media (max-width: 768px) {
+    margin-bottom: 20px; /* Adiciona espaço entre os cards em dispositivos móveis */
+  }
 `;
+
 export const Character_CardsContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 10px;
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(
+      1,
+      1fr
+    ); /* Um card por linha em dispositivos móveis */
+  }
 `;
--- src/assets/Styles/Seach_Section_Styles/Seach_Section_Styles.tsx
+++ src/assets/Styles/Seach_Section_Styles/Seach_Section_Styles.tsx
@@ -9,6 +9,15 @@ export const Wrapper = styled.div`
   height: 300px;
   border: 3px solid #000;
   background: #fff;
+
+  @media (max-width: 390px) {
+    flex-direction: column;
+    align-items: center; /* Centralize verticalmente em dispositivos móveis */
+    justify-content: center; /* Centralize horizontalmente em dispositivos móveis */
+    height: auto; /* Redefina a altura automática para se ajustar ao conteúdo */
+
+    height: 467px;
+  }
 `;
 
 export const Search_Section_Container = styled.div``;
@@ -17,17 +26,29 @@ export const InputAndIconContainer = styled.div`
   display: flex;
   align-items: center;
   width: 80%;
-  position: relative; /* Torna a posição relativa para que possamos posicionar o ícone de forma absoluta dentro dele */
+  position: relative;
+
+  @media (max-width: 390px) {
+    margin: auto;
+  }
 `;
 
 export const Text_Section = styled.h1`
   text-align: left;
   width: 70vh;
   margin-bottom: 20px;
+
+  @media (max-width: 390px) {
+    margin-bottom: 20px;
+    margin-top: 50px;
+    text-align: center;
+    font-size: 25px;
+    width: 100%;
+  }
 `;
 
 export const Search_Input = styled.input`
-  width: 100%; /* Preencha todo o espaço disponível */
+  width: 100%;
   padding: 10px;
   border: 1px solid #000;
   border-radius: 5px;
